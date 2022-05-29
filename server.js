048f3c5fbfca5c076b01d7a36bda40669a8e32b9
anastasiev <andrii.nastasiev@sigma.software>
2022-05-29 23:41:03 +0300

    set up heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 let express = require('express');
 let app = express();
-app.use(express.static('./dist/shop-page'));
+app.use(express.static('./dist/pnl-app'));
 app.get('/*', function (req, res) {
-  res.sendFile('index.html', {root: 'dist/shop-page/'}
+  res.sendFile('index.html', {root: 'dist/pnl-app/'}
   );
 });
 app.listen(process.env.PORT || 8080);
